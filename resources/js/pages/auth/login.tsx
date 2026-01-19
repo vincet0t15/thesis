1295a413e6864239b6171aeb714f3832e24f2288
vincet0t15 <vincet0t15@gmail.com>
2026-01-19 08:51:35 +0800

    asdf

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -36,7 +36,7 @@ export default function Login({ status, canResetPassword }: LoginProps) {
     };
 
     return (
-        <AuthLayout title="Log in to your account" description="Enter your username and password below to log in">
+        <AuthLayout title="Biometric-Based Attendance Monitoring System" description="Enter your username and password below to log in">
             <Head title="Log in" />
 
             <form className="flex flex-col gap-6" onSubmit={submit}>
